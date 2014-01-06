aba637d16ab46a66e17cf9b1886eada444429115
Sara Gorecki <sgorecki@gmail.com>
2014-01-06 16:11:28 -0500

    add starting files for rails app and start building routes/db/views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsEmails::Application.config.secret_token = '4bcad7114ca2d3442becdfb7c55c938743d5c45530343dc31419cd828f9b3658abd2b3734172526f2791b9bf4ebb45a8159220f8c805f52ea9a60fa1e36f0f35'
